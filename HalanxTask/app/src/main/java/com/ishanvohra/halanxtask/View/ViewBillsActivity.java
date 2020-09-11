e4d3745260e91059be01137722313ecf282fac4a
ishanvohra2 <ishanvohra2@gmail.com>
2020-09-11 16:01:35 +0530

    Initital commit

---
+++ HalanxTask/app/src/main/java/com/ishanvohra/halanxtask/View/ViewBillsActivity.java
@@ -0,0 +1,16 @@
+package com.ishanvohra.halanxtask.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ishanvohra.halanxtask.R;
+
+public class ViewBillsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_bills);
+    }
+}
